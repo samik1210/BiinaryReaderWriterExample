0a69008b461876141ed2ea0d1245bcd97727cd36
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:23:07 +0530

    Add project files.

---
+++ BiinaryReaderWriterExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace BiinaryReaderWriterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WriteBinaryFile();
+            ReadBinaryFile();
+            Console.ReadKey();
+        }
+        static void WriteBinaryFile()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("c:\\binaryfile.dat", FileMode.Create)))
+            {
+
+                writer.Write(12.5);
+                writer.Write("this is string data");
+                writer.Write(true);
+            }
+        }
+        static void ReadBinaryFile()
+        {
+            using (BinaryReader reader = new BinaryReader(File.Open("c:\\binaryfile.dat", FileMode.Open)))
+            {
+                Console.WriteLine("Double Value : " + reader.ReadDouble());
+                Console.WriteLine("String Value : " + reader.ReadString());
+                Console.WriteLine("Boolean Value : " + reader.ReadBoolean());
+            }
+        }
+    }
+}
+   
